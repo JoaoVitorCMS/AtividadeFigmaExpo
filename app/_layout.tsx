bc40099e5479a822ad4342b65d7639b8a60b2b98
João Vitor Costa de Moura Santos <joaovitorcms1@gmail.com>
2025-05-15 17:05:10 -0300

    Add files via upload

---
+++ app/_layout.tsx
@@ -0,0 +1,28 @@
+// import { Header } from '@/components/header';
+import { Stack } from 'expo-router';
+import 'react-native-reanimated';
+
+
+
+export default function RootLayout() {
+  // const [loaded] = useFonts({
+  //   SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  // });
+
+  // if (!loaded) {
+  //   // Async font loading only occurs in development.
+  //   return null;
+  // }
+
+  return (
+    <>
+    {/* <Header image={require("../assets/images/react-logo.png")}></Header> */}
+      <Stack>
+        <Stack.Screen name="index" options={{headerShown:false}}/>
+        <Stack.Screen name="register" options={{ headerShown: false}}/>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+    </>
+  );
+}
